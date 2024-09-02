cd084333377720dc6dca17adfc7aee075e47f5e4
Jesús Andrés Gutiérrez Arancivia <jesus.guti.arancivia@outlook.com>
2024-09-01 20:18:28 -0500

    Estructura de Proyecto
    Estructura de Proyecto

---
+++ TripBundle/src/main/java/pe/edu/upc/tripbundle/serviceimplements/UsersServiceImplement.java
@@ -0,0 +1,35 @@
+package pe.edu.upc.tripbundle.serviceimplements;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.tripbundle.entities.Users;
+import pe.edu.upc.tripbundle.repositories.IUsersRepository;
+import pe.edu.upc.tripbundle.serviceinterfaces.IUsersService;
+
+import java.util.List;
+
+@Service
+public class UsersServiceImplement implements IUsersService {
+    @Autowired
+    private IUsersRepository uR;
+
+    @Override
+    public List<Users> list() {
+        return uR.findAll();
+    }
+
+    @Override
+    public void insert(Users users) {
+        uR.save(users);
+    }
+
+    @Override
+    public void update(Users users) {
+        uR.save(users);
+    }
+
+    @Override
+    public void delete(int id) {
+        uR.deleteById(id);
+    }
+}
---
+++ TripBundle/src/main/java/pe/edu/upc/tripbundle/serviceinterfaces/ICityService.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.tripbundle.serviceinterfaces;
+
+import pe.edu.upc.tripbundle.entities.City;
+
+import java.util.List;
+
+public interface ICityService {
+    public List <City> list();
+}
